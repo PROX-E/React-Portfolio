1c373edc141ae0b5842a5e993ba7e91e853c8ca1
PROX-E <lichlord18@gmail.com>
2020-05-26 03:06:16 -0600

    create projects/portfolio page

---
+++ client/src/pages/projects/index.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+import Header from "../components/header";
+import Footer from "../components/footer";
+class Projects extends Component {
+    render() {
+        return 
+    }
+}
+
+export default Projects
