333cfe60a784850174f5e29a937e9ab49f5c2194
PROX-E <lichlord18@gmail.com>
2020-05-26 03:07:22 -0600

    basic nav for header
    change/ make more detailed later

--- client/src/components/header/index.js
+++ client/src/components/header/index.js
@@ -1,7 +1,23 @@
 import React from "react"
 
 function Header () {
-
-}
+        return (
+            <nav className="navbar navbar-expand-lg navbar-primary bg-dark navbar-fixed-top">
+                <a className="navbar-brand" href="/">
+                    <h2 className="text-white">Hunter Shriver</h2>
+                </a>
+                <div id="navbarNav">
+                    <ul className="navbar-nav">
+                        <li className="nav-item" id="home">
+                            <a className="nav-link" href="/"><button type="button" className="btn btn-danger text-white">Home</button></a>
+                        </li>
+                        <li className="nav-item" id="report">
+                            <a className="nav-link" href="/portfolio"><button type="button" className="btn btn-success text-white">Saved Books</button></a>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
+        );
+    }
 
 export default Header;
