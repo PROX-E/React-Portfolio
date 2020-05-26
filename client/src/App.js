75a1cfadf9bb680c5085539c735d2872f9ec7c5d
PROX-E <lichlord18@gmail.com>
2020-05-26 03:08:00 -0600

    import about page

--- client/src/App.js
+++ client/src/App.js
@@ -2,8 +2,10 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Header from "./components/header";
 import Footer from "./components/footer";
+import About from "./pages/about";
+import Projects from "./pages/projects"
 import './App.css';
-import About from "./pages/about"
+
 
 function App() {
   return (
